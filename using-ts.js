e699de49e1a6daae114fa3e77ea7e6a6999090ff
Sarang Kapadia <sarangksk@gmail.com>
2021-08-13 09:55:50 -0700

    minor changes

--- using-ts.js
+++ using-ts.js
@@ -7,3 +7,12 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+}
+
+console.log(fib(50));
