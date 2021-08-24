444f4457416ead47db34d0629cfbb188f1c65c43
Sarang Kapadia <sarangksk@gmail.com>
2021-08-24 15:22:54 -0700

    classes

--- myTSproject/app.js
+++ myTSproject/app.js
@@ -6,3 +6,50 @@ function onbuttonclick() {
 if (button !== null) {
     button.addEventListener('click', onbuttonclick);
 }
+var arrowFunc = function (button) { return console.log('hello arrow ' + button); };
+arrowFunc(button);
+var Point = /** @class */ (function () {
+    // private x: number | undefined;
+    // private y: number | undefined;
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+        // this.x = x;
+        // this.y = y;
+    }
+    Point.prototype.draw = function () {
+        // .. draw 
+        console.log("X = " + this.x + " and Y = " + this.y);
+    };
+    Object.defineProperty(Point.prototype, "X", {
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("Value of X < 0");
+            this.x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "Y", {
+        get: function () {
+            return this.y;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("Value of X < 0");
+            this.y = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Point;
+}());
+//let P = new Point();
+var P1 = new Point(11, 22);
+P1.draw();
+P1.X = 23;
+P1.Y = 24;
+P1.draw();
--- myTSproject/app.ts
+++ myTSproject/app.ts
@@ -7,3 +7,60 @@ function onbuttonclick(): void {
 if (button !== null) {
    button.addEventListener('click', onbuttonclick);
 }
+
+let arrowFunc = (button: HTMLButtonElement | null) => console.log('hello arrow ' + button);
+arrowFunc(button);
+
+class Point {
+
+   // private x: number | undefined;
+   // private y: number | undefined;
+
+   constructor(private x: number, private y: number) {
+      // this.x = x;
+      // this.y = y;
+   }
+
+   draw() {
+      // .. draw 
+      console.log(`X = ${this.x} and Y = ${this.y}`);
+   }
+
+   set X(value: number) {
+      if (value < 0)
+         throw new Error("Value of X < 0");
+      this.x = value;
+   }
+
+   get X() {
+      return this.x;
+   }
+
+   set Y(value: number) {
+      if (value < 0)
+         throw new Error("Value of X < 0");
+      this.y = value;
+   }
+
+   get Y() {
+      return this.y;
+   }
+}
+
+let P1 = new Point(11, 22);
+P1.draw();
+
+P1.X = 23;
+P1.Y = 24;
+P1.draw();
+
+
+const str = "heeabbacus";
+function longestPalindrome(input: string) {
+   const revStr = str.split("").reverse().join("");
+   for (let i: number = 0; i < str.length; i++) {
+
+   }
+
+}
+
